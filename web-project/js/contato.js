b7ae11e8aa71be491d9cae83dd466a633ae9cf5e
Vitor Hugo <vitorhugojoinville@gmail.com>
2019-10-22 16:46:56 -0300

    Validações na tela de contato, botão submit estilizado.

--- web-project/js/contato.js
+++ web-project/js/contato.js
@@ -1,4 +1,48 @@
 function validateForm() {
-    var form = document.forms[0].id;
-    console.log(form);
+    var nome = contactForm.nome.value;
+    var sobrenome = contactForm.sobrenome.value;
+    var email = contactForm.email.value;
+    var phone = contactForm.phone.value;
+    var info = contactForm.textinfo.value;
+    
+    // valida se o campo n esta vazio
+    if (nome=="") {
+        alert("Por favor, preencha o campo nome.");
+        contactForm.nome.focus();
+        return false;
+    
+    }
+
+    // valida se o campo n está vazio
+    if (sobrenome=="") {
+        alert("Por favor, preencha o campo sobrenome.");
+        contactForm.sobrenome.focus();
+        return false;
+    
+    }
+
+    // valida se o campo possui o @
+    if (email=="" || email.indexOf('@') == -1) {
+        alert("Por favor, preencha o campo e-mail.");
+        contactForm.email.focus();
+        return false;
+    
+    }
+
+    // valida se o campo possui no minimo 10 digitos
+    if (phone=="" || phone.length <= 10) {
+        alert("Por favor, preencha o campo celular.");
+        contactForm.phone.focus();
+        return false;
+    
+    }
+
+    // valida se o campo possui no minimo 50 caracteres
+    if (info.length <= 49 ) {
+        alert ("Por favor, preencha as informações com no mínimo 50 caracteres");
+        contactForm.info.focus();
+        return false;
+        
+    }
+
 }
