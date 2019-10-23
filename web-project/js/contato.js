bf430312d5f8efad15bba6e4c86c0819c2df4d13
Vitor Hugo <vitorhugojoinville@gmail.com>
2019-10-23 16:09:28 -0300

    Alteração na sequencia de validações entre email e celular.

--- web-project/js/contato.js
+++ web-project/js/contato.js
@@ -21,14 +21,6 @@ function validateForm() {
     
     }
 
-    // valida se o campo possui o @
-    if (email=="" || email.indexOf('@') == -1) {
-        alert("Por favor, preencha o campo e-mail.");
-        contactForm.email.focus();
-        return false;
-    
-    }
-
     // valida se o campo possui no minimo 10 digitos
     if (phone=="" || phone.length <= 10) {
         alert("Por favor, preencha o campo celular.");
@@ -37,9 +29,17 @@ function validateForm() {
     
     }
 
-    // valida se o campo possui no minimo 50 caracteres
+    // valida se o campo possui o @
+    if (email=="" || email.indexOf('@') == -1) {
+        alert("Por favor, preencha o campo e-mail.");
+        contactForm.email.focus();
+        return false;
+    
+    }
+
+        // valida se o campo possui no minimo 50 caracteres
     if (info.length <= 49 ) {
-        alert ("Por favor, preencha as informações com no mínimo 50 caracteres");
+        alert ("Por favor, preencha o campo informações com no mínimo 50 caracteres");
         contactForm.info.focus();
         return false;
         
